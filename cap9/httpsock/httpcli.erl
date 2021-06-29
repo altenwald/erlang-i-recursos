5649a48a949d84450d09f38d0a5dd7f417911573
Manuel Rubio <manuel@sentillia.com>
2021-06-29 19:32:39 +0200

    agregado ejemplo de nuevo capítulo crypto

---
+++ cap9/httpsock/httpcli.erl
@@ -0,0 +1,17 @@
+-module(httpcli).
+
+-export([http_request/2]).
+
+http_request(Port, URI) ->
+    {ok, Socket} = socket:open(inet, stream, tcp),
+    ok = socket:connect(Socket, #{
+        family => inet,
+        addr => {127,0,0,1},
+        port => Port
+    }),
+    Msg = "GET " ++ URI ++ " HTTP/1.1\r\nHost: localhost\r\n\r\n",
+    ok = socket:send(Socket, Msg),
+    ok = socket:shutdown(Socket, write),
+    {ok, Response} = socket:recv(Socket),
+    ok = socket:close(Socket),
+    Response.
